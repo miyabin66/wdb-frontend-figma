b8ed1d09b911f1c40a5bdf9b561e82fc53c727bd
miyabin66 <freezemark5166@gmail.com>
2023-01-14 15:42:47 +0900

    figmaと連携

--- chakra-theme.ts
+++ chakra-theme.ts
@@ -1,15 +1,22 @@
 import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
+import dtoken from './design-tokens.json';
 
 export const theme = extendTheme(
-  {
-    colors: {
-      brand: {
-        green: { 100: "#008508" }
-      }
-    }
-  },
+  { colors: tokenToColor() },
   withDefaultColorScheme({
     colorScheme: "brand",
   })
 );
+
 export default theme;
+
+function tokenToColor() {
+  return Object.fromEntries(
+    Object.entries(dtoken.color).map(([key, color]) => [
+      key,
+      Object.fromEntries(
+        Object.entries(color).map(([key, { value }]) => [key, value])
+      ),
+    ])
+  );
+}
--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@chakra-ui/react"
 export const Page = () => {
   return (
     <article>
-      <Button backgroundColor={"brand.green.100"} size={"lg"}>
+      <Button backgroundColor={"emerald.50"} size={"lg"}>
         Button
       </Button>
     </article>
